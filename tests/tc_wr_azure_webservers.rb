ed591e56f6d5af8bc0bd27372eff66e2fd731a6e
Chris Hudson <chudson@worldremit.com>
2017-08-16 13:54:56 +0100

    automated tests update

--- tests/tc_wr_azure_webservers.rb
+++ tests/tc_wr_azure_webservers.rb
@@ -7,12 +7,12 @@ MiniTest::Reporters.use! [MiniTest::Reporters::DefaultReporter.new,
 class TestWRAzureWebServers < MiniTest::Test
 
 	def test_servers_up
-		environment = 'dev'
-		client_name = 'armTemplateAutomation'
+		environment = 'prod'
+		client_name = 'wr-prd-app-csre-provision'
 		ENV['CSRE_LOG_LEVEL'] = 'DEBUG'
 		webserver_tester = WRAzureWebServers.new(client_name: client_name, environment: environment)
-		results = webserver_tester.check_wr_web_server_cluster('blue')
-		assert_operator results[:up].count, :>=, 1
+		results = webserver_tester.check_wr_web_servers()
+		assert_operator results[:up].count, :>=, 2
 		assert_operator results[:down].count, :==, 0
 	end
 end
