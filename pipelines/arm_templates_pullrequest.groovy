c1b4e46e2dfe1825303f19d2455b775379fe44f8
Chris Hudson <chudson@worldremit.com>
2017-10-18 09:53:48 +0100

    added p[olicies pipeline / job

--- pipelines/arm_templates_pullrequest.groovy
+++ pipelines/arm_templates_pullrequest.groovy
@@ -1,18 +1,36 @@
 try {
     setGitHubPullRequestStatus state: 'PENDING', context: "${env.JOB_NAME}", message: "Run #${env.BUILD_NUMBER} started"
     node {
-        stage ('Checkout from GitHub') {
-            checkout([$class: 'GitSCM', branches: [[name: '${GITHUB_PR_SOURCE_BRANCH}']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'arm_templates']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '2c74e2d9-4e99-4f84-86fc-affb2559331f', url: 'git@github.com:Worldremit/arm_templates.git']]])
+        stage ('Checkout branch from GitHub') {
+            checkout([$class: 'GitSCM', branches: [[name: '${GITHUB_PR_SOURCE_BRANCH}'], [name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'arm_templates']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '2c74e2d9-4e99-4f84-86fc-affb2559331f', url: 'git@github.com:Worldremit/arm_templates.git']]])
+        }
+        stage ('Checkout master from GitHub') {
+            checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'arm_templates-master']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '2c74e2d9-4e99-4f84-86fc-affb2559331f', url: 'git@github.com:Worldremit/arm_templates.git']]])
         }
         stage ('checkout test tools') {
             checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'infrastructure_test_suite']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '2c74e2d9-4e99-4f84-86fc-affb2559331f', url: 'git@github.com:chudsonwr/infrastructure_test_suite.git']]])
         }
         stage ('run some tests'){
-            sh "echo 'this is a test'"
-            sh "ls -lah && cd arm_templates && cat test_for_merge.txt"
-            sh "cd infrastructure_test_suite && ls -lah"
-            sh "printenv"
-            sh "ruby infrastructure_test_suite/tests/template/nsg_rules_tests.rb ./  ${env.GITHUB_PR_HEAD_SHA} $(git merge-base master ${env.GITHUB_PR_SOURCE_BRANCH'})"
+            masterCommit = sh(returnStdout: true, script: "cd arm_templates-master && git log -n 1 --pretty=format:'%H'").trim()
+            branchCommit = sh(returnStdout: true, script: "cd arm_templates && git log -n 1 --pretty=format:'%H'").trim()
+            println masterCommit
+            println branchCommit
+            sh "cd arm_templates && ruby ../infrastructure_test_suite/tests/template/templates_tests.rb ./  ${masterCommit} ${branchCommit}"
+            filesChanged = sh(returnStdout: true, script: "cd arm_templates && git diff --name-only ${masterCommit} ${branchCommit}").trim()
+            if (filesChanged.contains('/networks/')) {
+
+            }
+        }
+        stage ('set network update') {
+            fullPath =  sh(returnStdout: true, script: "pwd").trim()
+            networksToDeploy = new File("${fullPath}/arm_templates/networks_to_deploy.txt").text
+            println networksToDeploy
+        }
+        switch(networksToDeploy) {
+            case "nonprd":
+            stage ('Create NonPrd release in octopus') {
+                echo 'deploying to nonprd'
+            }
         }
         stage ('notify git hub') {
             setGitHubPullRequestStatus context: "${env.JOB_NAME}", message: 'SUCCESS', state: 'SUCCESS'
@@ -22,5 +40,3 @@ try {
     setGitHubPullRequestStatus context: "${env.JOB_NAME}", message: 'FAILED', state: 'FAILURE'
     throw e
 }
-    
-    
---
+++ pipelines/rbac_policies_pipeline.groovy
@@ -0,0 +1,33 @@
+node {
+  stage ('CheckoutRequiredRepoFromGitHub'){
+    //checkout hprod-migration repo   
+    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'infrastructure_test_suite']], gitTool: 'Default', submoduleCfg: [], userRemoteConfigs: [[url: 'git@github.com:chudsonwr/infrastructure_test_suite.git']]])
+    //chechout fps repo
+    checkout([$class: 'GitSCM', branches: [[name: "*/master"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'arm_templates']], gitTool: 'Default', submoduleCfg: [], userRemoteConfigs: [[url: 'git@github.com:Worldremit/arm_templates.git']]])
+    sh "cd infrastructure_test_suite/ && git submodule update --init --recursive"
+  }
+  withCredentials([string(credentialsId: 'github_PAC_chudson', variable: 'GIT_ACCESS_TOKEN'),
+  string(credentialsId: 'octopus-csre-app-wr', variable: 'AZURE_CLIENT_SECRET'),]) {
+    switch(env.action) {
+      case "deploy":
+      stage ('DeployPolicy'){
+        env.CSRE_LOG_LEVEL = "${log_level}"
+        sh "ruby infrastructure_test_suite/bin/provision.rb --action deploy_policy --environment ${subscription} --config ${policy_template} --complete --prep_templates"      
+      }
+      case "assign":
+      stage ('AssignPolicy'){
+        env.CSRE_LOG_LEVEL = "${log_level}"
+        sh "ruby infrastructure_test_suite/bin/provision.rb --action assign_policy --environment ${subscription} --config ${policy_template} --complete --prep_templates"      
+      }
+      case "deployAndAssign":
+      stage ('DeployPolicy'){
+        env.CSRE_LOG_LEVEL = "${log_level}"
+        sh "ruby infrastructure_test_suite/bin/provision.rb --action deploy_policy --environment ${subscription} --config ${policy_template} --complete --prep_templates"      
+      }
+      stage ('AssignPolicy'){
+        env.CSRE_LOG_LEVEL = "${log_level}"
+        sh "ruby infrastructure_test_suite/bin/provision.rb --action assign_policy --environment ${subscription} --config ${policy_template} --complete --prep_templates"      
+      }
+    }
+  }
+}
