2f68a22cd75ea8a503bbb450805e2a9f71b536a3
Chris Hudson <chudson@worldremit.com>
2017-10-18 10:04:06 +0100

    set env vars correctly

--- pipelines/rg_policies_pipeline.groovy
+++ pipelines/rg_policies_pipeline.groovy
@@ -6,27 +6,26 @@ node {
     checkout([$class: 'GitSCM', branches: [[name: "*/master"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'arm_templates']], gitTool: 'Default', submoduleCfg: [], userRemoteConfigs: [[url: 'git@github.com:Worldremit/arm_templates.git']]])
     sh "cd infrastructure_test_suite/ && git submodule update --init --recursive"
   }
-  withCredentials([string(credentialsId: 'github_PAC_chudson', variable: 'GIT_ACCESS_TOKEN'),
-  string(credentialsId: 'octopus-csre-app-wr', variable: 'AZURE_CLIENT_SECRET'),]) {
-    switch(env.action) {
-      case "deploy":
-      stage ('DeployPolicy'){
-        env.CSRE_LOG_LEVEL = "${log_level}"
-        sh "ruby infrastructure_test_suite/bin/provision.rb --action deploy_policy --environment ${subscription} --config ${policy_template} --complete --prep_templates"      
-      }
-      case "assign":
-      stage ('AssignPolicy'){
-        env.CSRE_LOG_LEVEL = "${log_level}"
-        sh "ruby infrastructure_test_suite/bin/provision.rb --action assign_policy --environment ${subscription} --config ${policy_template} --complete --prep_templates"      
-      }
-      case "deployAndAssign":
-      stage ('DeployPolicy'){
-        env.CSRE_LOG_LEVEL = "${log_level}"
-        sh "ruby infrastructure_test_suite/bin/provision.rb --action deploy_policy --environment ${subscription} --config ${policy_template} --complete --prep_templates"      
-      }
-      stage ('AssignPolicy'){
-        env.CSRE_LOG_LEVEL = "${log_level}"
-        sh "ruby infrastructure_test_suite/bin/provision.rb --action assign_policy --environment ${subscription} --config ${policy_template} --complete --prep_templates"      
+  withEnv(["CSRE_LOG_LEVEL=${log_level}"]) {
+    withCredentials([string(credentialsId: 'github_PAC_chudson', variable: 'GIT_ACCESS_TOKEN'),
+    string(credentialsId: 'octopus-csre-app-wr', variable: 'AZURE_CLIENT_SECRET'),]) {
+      switch(action) {
+        case "deploy":
+        stage ('DeployPolicy'){
+          
+          sh "ruby infrastructure_test_suite/bin/provision.rb --action deploy_policy --environment ${subscription} --config ${policy_template} --complete --prep_templates"      
+        }
+        case "assign":
+        stage ('AssignPolicy'){
+          sh "ruby infrastructure_test_suite/bin/provision.rb --action assign_policy --environment ${subscription} --config ${policy_template} --complete --prep_templates"      
+        }
+        case "deployAndAssign":
+        stage ('DeployPolicy'){
+          sh "ruby infrastructure_test_suite/bin/provision.rb --action deploy_policy --environment ${subscription} --config ${policy_template} --complete --prep_templates"      
+        }
+        stage ('AssignPolicy'){
+          sh "ruby infrastructure_test_suite/bin/provision.rb --action assign_policy --environment ${subscription} --config ${policy_template} --complete --prep_templates"      
+        }
       }
     }
   }
