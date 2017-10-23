c837b5c3b6001bf7aa8b69c7d20bbb3e03294071
Chris Hudson <chudson@worldremit.com>
2017-10-23 14:10:52 +0100

    updated pipeline for core networks deployment

--- pipelines/core_network.groovy
+++ pipelines/core_network.groovy
@@ -18,15 +18,19 @@ node {
     shortCommit = sh(returnStdout: true, script: "cd arm_templates && git log -n 1 --pretty=format:'%h'").trim()
     sh "zip core_networks.1.0.0.${BUILD_NUMBER}.zip core_network.json core_network.parameters.json"
   }
+  stage ('RunSomeTestsOnTheJson') {
+    withCredentials([string(credentialsId: 'github_PAC_chudson', variable: 'GIT_ACCESS_TOKEN'),
+    string(credentialsId: 'octopus-csre-app-wr', variable: 'AZURE_CLIENT_SECRET'),
+    string(credentialsId: 'core-storage-account-key', variable: 'AZURE_STORAGE_ACCOUNT_KEY'),]) {
+      env.CSRE_LOG_LEVEL = "${log_level}"
+      echo 'Testing Core templates'
+      sh "ruby infrastructure_test_suite/bin/provision.rb --action validate --output ./core_network.json --config arm_templates/networks/configs/networking_core.config.json --environment nonprd"
+    }
+    sh "ruby infrastructure_test_suite/tests/template/network_templates_tests.rb"
+  }
   stage ('PushDeployOctopus'){
     withCredentials([string(credentialsId: 'octopus_api_key', variable: 'octopus_api_key')]){
-      sh "ruby infrastructure_test_suite/scripts/create_octopus_release.rb --api_key ${octopus_api_key} -u https://octopusdeploy.worldremit.com -p deploy-core-network -e csre-core -f core_networks.1.0.0.${BUILD_NUMBER}.zip -s deploy_arm_template"
-    }
-  }
-  stage ('PushPackageToOctopus'){
-    withCredentials([string(credentialsId: 'octopus_api_key', variable: 'octopus_api_key')]){
-      //sh "/var/lib/octo/Octo push --package core_networks-${shortCommit}.zip --replace-existing --server https://octopusdeploy.worldremit.com --apiKey ${octopus_api_key}"
-      //sh "curl -X POST https://octopusdeploy.worldremit.com/api/packages/raw -H 'X-Octopus-ApiKey: ${octopus_api_key}' -F 'data=@core_networks-${shortCommit}.zip'"
+      //sh "ruby infrastructure_test_suite/scripts/create_octopus_release.rb -a ${octopus_api_key} -p deploy-core-network -e csre-core -f core_networks.1.0.0.${BUILD_NUMBER}.zip -s deploy_arm_template"
     }
   }
 }
