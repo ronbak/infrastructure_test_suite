af4c074961192695e1e58d4b4870a65b3f7c3742
Chris Hudson <chudson@worldremit.com>
2017-10-27 15:54:56 +0100

    updated provsioin script

--- scripts/provisionteamcity.sh
+++ scripts/provisionteamcity.sh
@@ -26,6 +26,9 @@ gem install simplecov
 gem install OptionParser
 gem install netaddr
 gem install git
+gem install bundler
+gem install json
+gem install rest-client
 
 
 wget https://download.jetbrains.com/teamcity/TeamCity-2017.1.5.tar.gz
@@ -41,10 +44,7 @@ useradd teamcity -s /bin/bash -m -g teamcity
 chown -R teamcity /opt/JetBrains/TeamCity  # Insert Username for $User    
 
 
-
-
-
-  
+ 
 #nano /etc/init.d/teamcity
 echo '#!/bin/sh' > /etc/init.d/teamcity
 echo '### BEGIN INIT INFO' >> /etc/init.d/teamcity
@@ -82,20 +82,16 @@ echo 'exit 0' >> /etc/init.d/teamcity
 wget https://xycsrecore01.blob.core.windows.net/bin/sqljdbc_6.2.2.0_enu.tar.gz
 
 gzip -d sqljdbc_6.2.2.0_enu.tar.gz
+mkdir /opt/JetBrains/TeamCity/.BuildServer/lib
+mkdir /opt/JetBrains/TeamCity/.BuildServer/lib/jdbc
 #tar -xzf sqljdbc_6.2.2.0_enu.tar.gz
-mv sqljdbc_6.2.2.0_enu.tar /opt/JetBrains/TeamCity/.BuildServer/lib/jdbc/sqljdbc_6.2.2.0_enu.tar
-cd /opt/JetBrains/TeamCity/.BuildServer/lib/jdbc/
+#mv sqljdbc_6.2.2.0_enu.tar /opt/JetBrains/TeamCity/.BuildServer/lib/jdbc/sqljdbc_6.2.2.0_enu.tar
+#cd /opt/JetBrains/TeamCity/.BuildServer/lib/jdbc/
 tar -xf sqljdbc_6.2.2.0_enu.tar
-cp sqljdbc_6.2/enu/mssql-jdbc-6.2.2.jre8.jar mssql-jdbc-6.2.2.jre8.jar
+cp sqljdbc_6.2/enu/mssql-jdbc-6.2.2.jre8.jar /opt/JetBrains/TeamCity/.BuildServer/lib/jdbc/mssql-jdbc-6.2.2.jre8.jar
 
 chmod +x /etc/init.d/teamcity
 update-rc.d teamcity defaults
-
-
-
-
-
-
 
 
 apt-get -y install nginx
@@ -104,94 +100,34 @@ cd /etc/nginx/sites-available
 rm default ../sites-enabled/default
 
 touch teamcity
-echo 'upstream app_server {' > teamcity
-echo '    server 127.0.0.1:8111 fail_timeout=0;' >> teamcity
-echo '}' >> teamcity
-echo ' ' >> teamcity
-echo 'server {' >> teamcity
-echo '    listen 443;' >> teamcity
-echo '    ssl on;' >> teamcity
-echo '    ssl_certificate /opt/JetBrains/TeamCity/.ssh/csreteamcity.pem;' >> teamcity
-echo '    ssl_certificate_key /opt/JetBrains/TeamCity/.ssh/csreteamcity_private_key.pem;' >> teamcity
-echo '    listen [::]:443 default ipv6only=on;' >> teamcity
-echo '    server_name csreteamcity.worldremit.com;' >> teamcity
-echo ' ' >> teamcity
-echo '    location / {' >> teamcity
-echo '        proxy_set_header X-Forwarded-Proto https;' >> teamcity
-echo '        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;' >> teamcity
-echo '        proxy_set_header Host $http_host;' >> teamcity
-echo '        proxy_redirect off;' >> teamcity
-echo ' ' >> teamcity
-echo '        if (!-f $request_filename) {' >> teamcity
-echo '            proxy_pass http://app_server;' >> teamcity
-echo '            break;' >> teamcity
-echo '        }' >> teamcity
-echo '    }' >> teamcity
+echo 'map $http_upgrade $connection_upgrade {' > teamcity
+echo '    default upgrade;' >> teamcity
+echo '    ''   '';' >> teamcity
 echo '}' >> teamcity
 echo '' >> teamcity
-
-
-
-
-echo 'http {' > teamcity
-echo '    # ... default settings here' >> teamcity
+echo 'server {' >> teamcity
+echo '' >> teamcity
+echo '    listen       443;' >> teamcity
+echo '    ssl on;' >> teamcity
+echo '    ssl_certificate /opt/JetBrains/TeamCity/.ssh/teamcity.csre.worldremit.com.pem;' >> teamcity
+echo '    ssl_certificate_key /opt/JetBrains/TeamCity/.ssh/teamcity.csre_private_key.pem;' >> teamcity
+echo '    server_name  localhost teamcity.csre.worldremit.com;' >> teamcity
+echo '' >> teamcity
 echo '    proxy_read_timeout     1200;' >> teamcity
 echo '    proxy_connect_timeout  240;' >> teamcity
-echo '    client_max_body_size   0;    # maximum size of an HTTP request. 0 allows uploading large artifacts to TeamCity' >> teamcity
-echo ' ' >> teamcity
-echo '    map $http_upgrade $connection_upgrade { # WebSocket support' >> teamcity
-echo '        default upgrade;' >> teamcity
-echo '        '' '';' >> teamcity
-echo '    }' >> teamcity
-echo '     ' >> teamcity
-echo '    server {' >> teamcity
-echo '        listen       443; # public server port' >> teamcity
-echo '        ssl on;' >> teamcity
-echo '        ssl_certificate /opt/JetBrains/TeamCity/.ssh/csreteamcity.pem;' >> teamcity
-echo '        ssl_certificate_key /opt/JetBrains/TeamCity/.ssh/csreteamcity_private_key.pem;' >> teamcity
-echo '        listen [::]:443 default ipv6only=on;' >> teamcity
-echo '        server_name csreteamcity.worldremit.com;' >> teamcity
-echo '     ' >> teamcity
-echo '        location /tc { # public context (should be the same as internal context)' >> teamcity
-echo '            proxy_pass          http://127.0.0.1:8111/tc; # full internal address' >> teamcity
-echo '            proxy_http_version  1.1;' >> teamcity
-echo '            proxy_set_header    Host $server_name:$server_port;' >> teamcity
-echo '            proxy_set_header    X-Forwarded-Host $http_host;    # necessary for proper absolute redirects and TeamCity CSRF check' >> teamcity
-echo '            proxy_set_header    X-Forwarded-Proto $scheme;' >> teamcity
-echo '            proxy_set_header    X-Forwarded-For $remote_addr;' >> teamcity
-echo '            proxy_set_header    Upgrade $http_upgrade; # WebSocket support' >> teamcity
-echo '            proxy_set_header    Connection $connection_upgrade; # WebSocket support' >> teamcity
-echo '        }' >> teamcity
+echo '    client_max_body_size   0;' >> teamcity
+echo '' >> teamcity
+echo '    location / {' >> teamcity
+echo '' >> teamcity
+echo '        proxy_pass          http://localhost:8111/;' >> teamcity
+echo '        proxy_http_version  1.1;' >> teamcity
+echo '        proxy_set_header    X-Forwarded-For $remote_addr;' >> teamcity
+echo '        proxy_set_header    X-Forwarded-Host $http_host;' >> teamcity
+echo '        proxy_set_header    Host $server_name:$server_port;' >> teamcity
+echo '        proxy_set_header    Upgrade $http_upgrade;' >> teamcity
+echo '        proxy_set_header    Connection $connection_upgrade;' >> teamcity
 echo '    }' >> teamcity
 echo '}' >> teamcity
-echo '' >> teamcity
 
 ln -s /etc/nginx/sites-available/teamcity /etc/nginx/sites-enabled/
 service nginx restart
-
-
-
-map $http_upgrade $connection_upgrade {
-    default upgrade;
-    ''   '';
-}
-
-server {
-
-    listen       80;
-    server_name  localhost csreteamcity.worldremit.com;
-
-    proxy_read_timeout     1200;
-    proxy_connect_timeout  240;
-    client_max_body_size   0;
-
-    location / {
-
-        proxy_pass          http://localhost:8111/;
-        proxy_http_version  1.1;
-        proxy_set_header    X-Forwarded-For $remote_addr;
-        proxy_set_header    Host $server_name:$server_port;
-        proxy_set_header    Upgrade $http_upgrade;
-        proxy_set_header    Connection $connection_upgrade;
-    }
-}
