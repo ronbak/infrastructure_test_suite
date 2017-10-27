47068bcee88cbbff4a8ba0851b935fd91f97ff5e
Chris Hudson <chudson@worldremit.com>
2017-10-27 11:17:58 +0100

    updated tests and rake files for TC

--- tests/template/templates_tests.rb
+++ tests/template/templates_tests.rb
@@ -60,6 +60,8 @@ class TestWRTemplate <  MiniTest::Test
       if template.first.include?('/nsg_rules/')
         puts "Performing a NSG rules template test set"
         tester = WRTemplatesTester.new(template.first)
+        puts template.first
+        puts "#{File.dirname(template.first)}/../configs/*"
         subnet_info = tester.allowed_subnet_names("#{File.dirname(template.first)}/../configs/*")
         puts "These are your allowed subnet names: #{subnet_info}"
         rules_array = tester.template['resources']
