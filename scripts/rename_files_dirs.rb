8bd3f68284025224152563e371750f3ab4160ea5
Chris Hudson <chudson@worldremit.com>
2017-12-19 15:56:44 +0000

    added renaming script

--- scripts/rename_files_dirs.rb
+++ scripts/rename_files_dirs.rb
@@ -1,14 +1,33 @@
 projectName = ARGV[0]
 directory = ARGV[1]
 
-Dir.glob("#{directory}/**/*").reverse.each do |file|
-  if File.directory?(file)
-    File.rename(file, file.gsub('baseProject', projectName)) #or upcase if you want to convert to uppercase
-  end
-end
+base_names = ['baseProject', 'BaseProject']
 
-Dir.glob("#{directory}/**/*").reverse.each do |file|
-  if File.file?(file) && file.include?('baseProject')
-    File.rename(file, file.gsub('baseProject', projectName)) #or upcase if you want to convert to uppercase
+# For each name
+base_names.each do |base_name|
+  # Rename any directories 
+  Dir.glob("#{directory}/**/*").reverse.each do |path|
+    if File.directory?(path)
+      File.rename(path, path.gsub(base_name, projectName)) #or upcase if you want to convert to uppercase
+    end
+  end
+
+  # Rename any files
+  Dir.glob("#{directory}/**/*").reverse.each do |file|
+    if File.file?(file) && file.include?(base_name)
+      new_name = file.gsub(base_name, projectName)
+      File.rename(file, new_name) 
+    end
+  end
+
+  # Rename any strings within xml files
+  Dir.glob("#{directory}/**/*").reverse.each do |file|
+    if File.file?(file) && file[-4..-1].eql?('.xml')
+      text = File.read(file)
+      if text.include?(base_name)
+        new_contents = text.gsub(base_name, projectName)
+        File.open(file_name, "w") {|file| file.write new_contents }
+      end
+    end
   end
 end
