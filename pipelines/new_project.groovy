53502c68d239ae09b55167ea04951e8a0abac08f
Chris Hudson <chudson@worldremit.com>
2017-12-20 16:21:07 +0000

    updated pipeline code to now create a new repo i Git for the code

--- pipelines/new_project.groovy
+++ pipelines/new_project.groovy
@@ -1,13 +1,57 @@
 import groovy.json.*
 node{
   stage('clone_repo'){
-
+    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'infrastructure_test_suite']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '2c74e2d9-4e99-4f84-86fc-affb2559331f', url: 'git@github.com:chudsonwr/infrastructure_test_suite.git']]])
+    sh(returnStdout: true, script: "if [ -d source_repo ]; then rm -rf source_repo; fi")
+    sh(returnStdout: true, script: "git clone $sourceRepo source_repo")
+    sh(returnStdout: true, script: "if [ -d target_repo ]; then rm -rf target_repo; fi")
+    sh(returnStdout: true, script: "cp -r source_repo target_repo")
+    sh(returnStdout: true, script: "cd target_repo && rm -rf .git/")
   }
-  stage('replace_names'){
+  stage('replace_names_TCBuild'){
+    // def workspace = env.WORKSPACE 
+    // def currentDir = new File(workspace + "/target_repo/.teamcity/");
+    // def fileText;
+    // def exts = [".xml"]
+    // def srcExp = "baseProject"
+    // def replaceText = "$projectName"
+    // currentDir.eachFileRecurse(
+    //   {file ->
+    //     println file.name
+    //     for (ext in exts){
+    //       if (file.name.endsWith(ext)) {
+    //         println "we're changing the file: ${file.name}"
+    //         fileText = file.text;
+    //         fileText = fileText.replaceAll(srcExp, replaceText)
+    //         file.write(fileText);
+    //       }
+    //     }
+    //   }
+    // )
 
+    // def currentDir = new File("./target_repo/.teamcity/");
+    // def fileText;
+    // def exts = [".xml"]
+    // def srcExp = "baseProject"
+    // def replaceText = "NewProject1"
+    // currentDir.eachFileRecurse(
+    //   {file ->
+    //     if (file.name.includes(srcExp)) {
+    //       println 'we are doing it'
+    //       println file.name
+    //       newName = file.path.replaceAll(srcExp, replaceText)
+    //       println newName
+    //       file.renameTo(newName)
+    //     }
+
+    //   }
+    // )
+    sh(resturnStdout: true, script: "ruby infrastructure_test_suite/scripts/rename_files_dirs.rb $projectName target_repo/.teamcity")
   }
-  stage('create_resource_groups'){
-
+  stage('commit_to_git'){
+    withCredentials([string(credentialsId: 'Github_PAC_csreautomation', variable: 'GITHUB_ACCESS_TOKEN'),]){
+      sh(resturnStdout: true, script: "curl -v -i -H 'Authorization: token ${env.GITHUB_ACCESS_TOKEN} -d '{\"name\":\"${projectName}\"' https://api.github.com/user/repos")
+    }
   }
   stage('create_octopus_project'){
     withCredentials([string(credentialsId: 'octokey', variable: 'OCTOPUS_API_KEY'),]){
@@ -36,7 +80,7 @@ node{
       // create new octopus job with name under selected projectgroup 
       def post = new URL("https://octopusdeploy.worldremit.com/api/projects?clone=$cloneProj.Id").openConnection();
       message_map = [:]
-      message_map.Name = name
+      message_map.Name = projectName
       message_map.ProjectGroupId = obj.Id 
       message_map.LifecycleId = 'Lifecycles-1'
       def message_string = JsonOutput.toJson(message_map)
